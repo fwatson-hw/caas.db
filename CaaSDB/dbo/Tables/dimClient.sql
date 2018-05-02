1bac4b5a4ce92b3ebe8b2f55a39dd7159e1d12b4
hw\fwatson <fwatson@healthwise.org>
2018-05-02 11:26:45 -0600

    updates to dimclient - using keyid instead of defaultcontentconfigurationid

--- CaaSDB/dbo/Tables/dimClient.sql
+++ CaaSDB/dbo/Tables/dimClient.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [dbo].[DimClient] (
     [ClientKey]             INT            IDENTITY (1, 1) NOT NULL,
     [ClientToken]           NVARCHAR (255) NOT NULL,
-    [DefaultContentConfigurationId]                 NVARCHAR (500) NOT NULL,
+    [KeyId]                 NVARCHAR (500) NOT NULL,
     [ClientName]            NVARCHAR (255) NOT NULL,
-    [UrlAlias]              NVARCHAR (500) NOT NULL,
+    [UrlAlias]              NVARCHAR (50) NOT NULL,
     [IsInferred]            BIT            CONSTRAINT [DF_DimClient_IsInferred] DEFAULT ((0)) NOT NULL,
     [CreatedDate]           DATETIME2 (7)  NOT NULL,
     [CreatedByPackageId]    INT            NOT NULL,
 	CONSTRAINT PK_DimClient PRIMARY KEY CLUSTERED(ClientKey)
 );
 GO
-CREATE NONCLUSTERED INDEX IX_DimClient_Lookup ON dbo.DimClient(ClientToken, DefaultContentConfigurationId);
+CREATE NONCLUSTERED INDEX IX_DimClient_Lookup ON dbo.DimClient(ClientToken, KeyId);
 GO
 CREATE NONCLUSTERED INDEX IX_DimClient_Inferred ON dbo.DimClient(IsInferred, ClientKey);
 GO
