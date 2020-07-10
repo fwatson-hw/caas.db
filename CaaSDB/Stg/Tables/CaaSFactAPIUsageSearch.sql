2765293e7ea06ba4d6a6e49e836bb142e944f11c
kyson <kchin@healthwise.org>
2020-07-10 16:48:37 -0600

    Added tables around the Search API

---
+++ CaaSDB/Stg/Tables/CaaSFactAPIUsageSearch.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Stg].[CaaSFactAPIUsageSearch] (
+    [FactAPIUsageSearchKey] BIGINT IDENTITY (1, 1) NOT NULL,
+    [DateKey]               INT    NOT NULL,
+    [ClientKey]             INT    NOT NULL,
+    [CodeFamilyKey]         INT    NOT NULL,
+    [SearchTermKey]         INT    NOT NULL,
+    [StatusKey]             INT    NOT NULL,
+    [Requests]              INT    NOT NULL,
+    CONSTRAINT [PK_CaasFactAPIUsageSearch] PRIMARY KEY CLUSTERED ([FactAPIUsageSearchKey] ASC)
+);
+
---
+++ CaaSDB/dbo/Tables/DimSearchTerm.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DimSearchTerm] (
+    [SearchTermKey]      INT            IDENTITY (1, 1) NOT NULL,
+    [SearchTerm]         NVARCHAR (100) NOT NULL,
+    [IsInferred]         BIT            CONSTRAINT [DF_DimSearchTerm_IsInferred] DEFAULT ((0)) NOT NULL,
+    [CreatedDate]        DATETIME2 (7)  NOT NULL,
+    [CreatedByPackageId] INT            NOT NULL,
+    CONSTRAINT [PK_DimSearchTerm] PRIMARY KEY CLUSTERED ([SearchTermKey] ASC)
+);
+
