4559f1e725668a61df1b71031640de6ee767e158
kyson <kchin@healthwise.org>
2020-07-16 09:02:08 -0600

    Added Stg.StructuredContent_concept

---
+++ CaaSDB/Stg/Tables/StructuredContent_concept.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [Stg].[StructuredContent_concept] (
+    [datetime_dt]           NVARCHAR (50)  NULL,
+    [codesicd10cmR1_ss]     NVARCHAR (MAX) NULL,
+    [conceptbucket_txt]     NVARCHAR (MAX) NULL,
+    [codesicd9cmR1_ss]      NVARCHAR (MAX) NULL,
+    [codesnuccR1_ss]        NVARCHAR (MAX) NULL,
+    [contents_t]            NVARCHAR (MAX) NULL,
+    [weak_en]               NVARCHAR (MAX) NULL,
+    [keywordsESR1_ss]       NVARCHAR (MAX) NULL,
+    [medicalSpecialties_ss] NVARCHAR (400) NULL,
+    [alternativeLabels_ss]  NVARCHAR (400) NULL,
+    [childrenAndRelated_ss] NVARCHAR (400) NULL,
+    [label_s]               NVARCHAR (100) NULL,
+    [strong_en]             NVARCHAR (100) NULL,
+    [label_t]               NVARCHAR (100) NULL,
+    [concept_id_s]          NVARCHAR (50)  NULL,
+    [medium_en]             NVARCHAR (50)  NULL,
+    [taxon_s]               NVARCHAR (20)  NULL,
+    [keywordsENR1_ss]       NVARCHAR (MAX) NULL,
+    [id]                    NVARCHAR (50)  NULL,
+    [codessnomedctR1_ss]    NVARCHAR (MAX) NULL,
+    [pubhub_version_s]      NVARCHAR (10)  NULL,
+    [status_s]              NVARCHAR (10)  NULL,
+    [keywordsFRR1_ss]       NVARCHAR (MAX) NULL,
+    [_version_]             NVARCHAR (100) NULL
+);
+
