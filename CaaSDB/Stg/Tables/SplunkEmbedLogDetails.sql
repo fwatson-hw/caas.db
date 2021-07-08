fd87ed2e98e8c6ab8435b7e99b4bd990a9c76ddf
T-FWatson <T-FWatson@test.hwenv.net>
2021-07-08 07:14:42 -0600

    update SplunkEmbedLogDetails table to allow 100 characters into TopicId fields

--- CaaSDB/Stg/Tables/SplunkEmbedLogDetails.sql
+++ CaaSDB/Stg/Tables/SplunkEmbedLogDetails.sql
@@ -3,7 +3,7 @@
     EventDateTime      DATETIME        NULL,
     [cs_Referrer] NVARCHAR (4000) NULL,
     [ecode]      NVARCHAR (500)  NULL,
-    [topicId]    NVARCHAR (50)   NULL,
+    [topicId]    NVARCHAR (100)   NULL,
     [locale]     NVARCHAR (50)   NULL,
     [sc_status]  INT             NULL,
     [requests]   INT             NULL,
